219540fa3c45e5d34f136c9f3f21e1caa332f3e6
johnny17 <suiyi8760@163.com>
2017-10-12 20:09:42 +0800

    添加redux插件支持

--- demo7-todo/src/store/Store.js
+++ demo7-todo/src/store/Store.js
@@ -1,11 +1,29 @@
-import {createStore,combineReducers} from 'redux'
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux'
+// import Perf from 'react-addons-perf'
 
 import {Reducer as TodoReduce} from '../todos'
 import {Reducer as FilterReduce} from '../filter'
 
+const win = window
+/*//供perf-react性能监控组件调用(暂不支持react16)
+win.Perf = Perf*/
+
 const reducer = combineReducers({
-    todo:TodoReduce,
-    filter:FilterReduce
+    todo: TodoReduce,
+    filter: FilterReduce
 })
 
-export default createStore(reducer, {})
+const middlewares = []
+//用于开发环境下检查reducer是否为纯函数(不直接修改state)
+if (process.env.NODE_ENV !== 'production') {
+    //push进中间件数组供applyMiddleware调用
+    middlewares.push(require('redux-immutable-state-invariant').default())
+}
+
+//store增强,compose用于将多个增强合并一起
+const storeEnhancers = compose(
+    applyMiddleware(...middlewares),
+    (win && win.devToolsExtension) ? win.devToolsExtension() : f => f
+)
+
+export default createStore(reducer, {}, storeEnhancers)
