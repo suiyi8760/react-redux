d4136baf48b0863b73baa86b940a80755cd4aa0d
johnny17 <suiyi8760@163.com>
2017-10-19 17:40:23 +0800

    change title

--- demo9-HOC/src/App.js
+++ demo9-HOC/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">TODO LIST DEMO</h1>
+                    <h1 className="App-title">Countdown HOC DEMO</h1>
                 </header>
                 <Countdown beginValue={10}>
                     {(count) => (<div>{count > 0 ? count : 'Happy New Year'}</div>)}
